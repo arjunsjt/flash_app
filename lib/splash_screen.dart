271cc25cce676922146772a93944147a8ffb3386
Noel Binoy <noelzakbin03@gmail.com>
2023-06-22 16:59:34 +0400

    Final Commit splashscreen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,3 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:marquee/marquee.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: (MainAxisSize.min),
+        children: [
+          Image.asset(
+            'assets/images/logo2.png',
+            scale: 0.5,
+            fit: BoxFit.fitWidth,
+          ),
+          const SizedBox(height: 5),
+          const Text(
+            'FlashApp',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Color.fromARGB(255, 208, 211, 15),
+                fontSize: 50,
+                fontFamily: 'Righteous'),
+          ),
+        ],
+      ),
+    );
+  }
+}
