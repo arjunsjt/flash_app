8083a0ca3dc45160e983ddb4931e5916af7ff40c
Noel Binoy <noelzakbin03@gmail.com>
2023-08-15 10:18:41 +0530

    Drawer UI along with info button

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flash_app/splash_screen.dart';
 import 'package:flash_app/colors.dart';
+import 'package:flash_app/home_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
-        home: Scaffold(
+      /*home: Scaffold(
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(colors: [
@@ -13,8 +14,8 @@ void main() {
             AppColors.richBlack,
           ]),
         ),
-        child: const SplashScreen(),
-      ),
-    )),
+        child: const HomeScreen(),*/
+      home: const HomeScreen(),
+    ),
   );
 }
